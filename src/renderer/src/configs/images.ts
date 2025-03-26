c6afd01bac054372f279e9085249a1fffaa5af41
tdn352001 <tdn352001@gmail.com>
2025-03-26 15:49:22 +0700

    Add branding images configuration

---
+++ src/renderer/src/configs/images.ts
@@ -0,0 +1,13 @@
+import CORE_NODE_LOGO from '@assets/images/branding/core-node.svg'
+import PI from '@assets/images/branding/pi.svg'
+import TOKEN from '@assets/images/branding/token.svg'
+
+const IMAGES = {
+  BRANDING: {
+    CORE_NODE_LOGO,
+    PI,
+    TOKEN
+  }
+}
+
+export default IMAGES
