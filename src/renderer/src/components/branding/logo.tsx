b7ef4b2e29a91456ea09896f04de0f731d3605d4
anonsyn <tdn352001@gmail.com>
2025-04-25 13:13:27 +0700

    Update logo component

--- src/renderer/src/components/branding/logo.tsx
+++ src/renderer/src/components/branding/logo.tsx
@@ -2,13 +2,15 @@ import IMAGES from '@/configs/images'
 import { cn } from '@/utils/tw'
 import { ImgHTMLAttributes } from 'react'
 
-interface LogoProps extends ImgHTMLAttributes<HTMLImageElement> {}
+interface LogoProps extends ImgHTMLAttributes<HTMLImageElement> {
+  horizontal?: boolean
+}
 
-const Logo = ({ className }: LogoProps) => {
+const Logo = ({ className, horizontal }: LogoProps) => {
   return (
     <img
       className={cn('max-h-none w-auto max-w-none', className)}
-      src={IMAGES.BRANDING.CORE_NODE_LOGO}
+      src={horizontal ? IMAGES.BRANDING.CORE_NODE_LOGO_HORIZONTAL : IMAGES.BRANDING.CORE_NODE_LOGO}
       alt="logo"
     />
   )
--- src/renderer/src/configs/images.ts
+++ src/renderer/src/configs/images.ts
@@ -1,3 +1,4 @@
+import CORE_NODE_LOGO_HORIZONTAL from '@assets/images/branding/core-node-horizontal.svg'
 import CORE_NODE_LOGO from '@assets/images/branding/core-node.svg'
 import PI from '@assets/images/branding/pi.svg'
 import TOKEN from '@assets/images/branding/token.svg'
@@ -5,6 +6,7 @@ import TOKEN from '@assets/images/branding/token.svg'
 const IMAGES = {
   BRANDING: {
     CORE_NODE_LOGO,
+    CORE_NODE_LOGO_HORIZONTAL,
     PI,
     TOKEN
   }
