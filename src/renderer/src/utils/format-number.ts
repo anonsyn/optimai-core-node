77f1d43371a417166fb4ac0126005942b1477529
anonsyn <tdn352001@gmail.com>
2025-04-26 23:44:59 +0700

    Add format number utils

---
+++ src/renderer/src/utils/format-number.ts
@@ -0,0 +1,17 @@
+interface CompactNumberOptions {
+  minimumFractionDigits?: number
+  maximumFractionDigits?: number
+}
+export const formatNumber = (value: number, options?: CompactNumberOptions) => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: options?.minimumFractionDigits || 0,
+    maximumFractionDigits: options?.maximumFractionDigits || 4
+  })
+
+  return formatter.format(value)
+}
+
+export const balanceFormatOptions: CompactNumberOptions = {
+  minimumFractionDigits: 4,
+  maximumFractionDigits: 4
+}
