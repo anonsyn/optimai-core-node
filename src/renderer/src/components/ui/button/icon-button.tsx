9481dff79faafa38f590f997eabda740e1bdbc6f
anonsyn <tdn352001@gmail.com>
2025-04-26 23:51:58 +0700

    Add icon button

---
+++ src/renderer/src/components/ui/button/icon-button.tsx
@@ -0,0 +1,22 @@
+import { cn } from '@/utils/tw'
+import React from 'react'
+
+export type IconButtonProps = React.ComponentPropsWithoutRef<'button'> & {
+  isSubmitting?: boolean
+}
+
+const IconButton = ({ className, children, ...props }: IconButtonProps) => {
+  return (
+    <button
+      className={cn(
+        'border-foreground/10 bg-accent/50 backdrop-blur-40 hover:border-foreground/20 hover:bg-accent flex size-9 items-center justify-center rounded-full border transition-colors',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default IconButton
