9f0c5b93d599011dcb43a258cff0eb97fa859866
anonsyn <tdn352001@gmail.com>
2025-04-26 20:08:55 +0700

    Update env config

--- src/renderer/src/configs/env.ts
+++ src/renderer/src/configs/env.ts
@@ -1,9 +1,9 @@
 // URLS
-const BASE_API_URL = process.env.EXPO_PUBLIC_API_URL || 'https://api.optimai.network'
-const BASE_DASHBOARD_URL = process.env.EXPO_PUBLIC_DASHBOARD_URL || 'https://node.optimai.network'
-const BASE_LP_URL = process.env.EXPO_PUBLIC_LP_URL || 'https://optimai.network'
+const BASE_API_URL = import.meta.env.VITE_API_URL || 'https://api.optimai.network'
+const BASE_DASHBOARD_URL = import.meta.env.VITE_DASHBOARD_URL || 'https://node.optimai.network'
+const BASE_LP_URL = import.meta.env.VITE_LP_URL || 'https://optimai.network'
 
-const DEVICE_SECRET = process.env.EXPO_PUBLIC_DEVICE_SECRET || 'mob-secret-key'
-const CLIENT_APP_ID = process.env.EXPO_PUBLIC_CLIENT_APP_ID || ''
+const DEVICE_SECRET = import.meta.env.VITE_DEVICE_SECRET || 'mob-secret-key'
+const CLIENT_APP_ID = import.meta.env.VITE_CLIENT_APP_ID || ''
 
 export { BASE_API_URL, BASE_DASHBOARD_URL, BASE_LP_URL, CLIENT_APP_ID, DEVICE_SECRET }
