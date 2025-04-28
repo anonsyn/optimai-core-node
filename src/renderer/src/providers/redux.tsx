ebcbf842bd7bd53d35468f859bf82df13ec678c0
anonsyn <tdn352001@gmail.com>
2025-04-28 13:55:41 +0700

    Implement redux persist

--- src/renderer/src/providers/redux.tsx
+++ src/renderer/src/providers/redux.tsx
@@ -1,9 +1,16 @@
-import { store } from '@/store'
+import { persistor, store } from '@/store'
 import { PropsWithChildren } from 'react'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const ReduxProvider = ({ children }: PropsWithChildren) => {
-  return <Provider store={store}>{children}</Provider>
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  )
 }
 
 export default ReduxProvider
--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -3,16 +3,37 @@ import headerSlice from '@/store/slices/header'
 import modalSlice from '@/store/slices/modals'
 import onlineSlice from '@/store/slices/online'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import checkInSlice from './slices/checkin'
-export const store = configureStore({
-  reducer: combineReducers({
-    [authSlice.name]: authSlice.reducer,
-    [modalSlice.name]: modalSlice.reducer,
-    [headerSlice.name]: headerSlice.reducer,
-    [onlineSlice.name]: onlineSlice.reducer,
-    [checkInSlice.name]: checkInSlice.reducer
-  })
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: [authSlice.name]
+}
+
+const rootReducer = combineReducers({
+  [authSlice.name]: authSlice.reducer,
+  [modalSlice.name]: modalSlice.reducer,
+  [headerSlice.name]: headerSlice.reducer,
+  [onlineSlice.name]: onlineSlice.reducer,
+  [checkInSlice.name]: checkInSlice.reducer
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE']
+      }
+    })
+})
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
