ed96bf829235dfd8b8e2149b56805400ae3a7a60
anonsyn <tdn352001@gmail.com>
2025-04-28 09:20:23 +0700

    Add animated number component

---
+++ src/renderer/src/components/ui/animated-number.tsx
@@ -0,0 +1,17 @@
+import { balanceFormatOptions } from '@/utils/format-number'
+import NumberFlow from '@number-flow/react'
+
+interface AnimatedNumberProps {
+  value?: number
+  format?: Omit<Intl.NumberFormatOptions, 'notation'> & {
+    notation?: Exclude<Intl.NumberFormatOptions['notation'], 'scientific' | 'engineering'>
+  }
+}
+
+const AnimatedNumber = ({ value, format }: AnimatedNumberProps) => {
+  return (
+    <NumberFlow value={value || 0} trend="increasing" format={format || balanceFormatOptions} />
+  )
+}
+
+export default AnimatedNumber
