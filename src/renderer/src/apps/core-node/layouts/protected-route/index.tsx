9e97a725428b068dea6723b95570b8952e5d9d83
anonsyn <tdn352001@gmail.com>
2025-11-02 20:14:49 +0700

    fix(protected-route): enforce authentication check in ProtectedRoute component
    - Restore the authentication check in the ProtectedRoute component to redirect unauthenticated users to the startup path.
    - Utilize the useAppSelector hook to access the authentication state from the Redux store.
    
    These changes ensure that only signed-in users can access protected routes, enhancing application security.

--- src/renderer/src/apps/core-node/layouts/protected-route/index.tsx
+++ src/renderer/src/apps/core-node/layouts/protected-route/index.tsx
@@ -1,11 +1,14 @@
-import { Outlet } from 'react-router'
+import { useAppSelector } from '@/hooks/redux'
+import { authSelectors } from '@/store/slices/auth'
+import { PATHS } from '@core-node/routers/paths'
+import { Navigate, Outlet } from 'react-router'
 
 const ProtectedRoute = () => {
-  // const isSignedIn = useAppSelector(authSelectors.isSignedIn)
+  const isSignedIn = useAppSelector(authSelectors.isSignedIn)
 
-  // // if (!isSignedIn) {
-  // //   return <Navigate to={PATHS.START_UP} />
-  // // }
+  if (!isSignedIn) {
+    return <Navigate to={PATHS.START_UP} />
+  }
   return <Outlet />
 }
 
